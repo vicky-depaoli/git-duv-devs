2d26af0cbeb5b8c34884c405046d24b8bc0e9e8b
vicky-depaoli <depaoli.vicky@gmail.com>
2026-05-20 12:07:51 +0200

    Add book list and detail views

--- frontend/src/app/components/book-detail/book-detail.ts
+++ frontend/src/app/components/book-detail/book-detail.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { BookService } from '../../services/book';
+import { Book } from '../../models/book';
 
 @Component({
   selector: 'app-book-detail',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './book-detail.html',
-  styleUrl: './book-detail.css',
+  styleUrl: './book-detail.css'
 })
-export class BookDetail {
+export class BookDetail implements OnInit {
+  book = signal<Book | null>(null);
+  errorMessage = signal('');
 
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.bookService.getBookById(id).subscribe({
+      next: (data) => this.book.set(data),
+      error: () => this.errorMessage.set('No se pudo cargar el detalle del libro')
+    });
+  }
 }
