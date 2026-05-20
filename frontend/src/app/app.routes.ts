24983997b320f4b84989fbb365d5d7482bfaef7f
Matías <mcambar2610@g.educaand.es>
2026-05-20 12:29:38 +0200

    book-form

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { BookDetail } from './components/book-detail/book-detail';
 import { BookForm } from './components/book-form/book-form';
 import { AuthorList } from './components/author-list/author-list';
 
+
 export const routes: Routes = [
   { path: '', component: BookList },
   { path: 'books/:id', component: BookDetail },
   { path: 'new-book', component: BookForm },
-  { path: 'authors', component: AuthorList }
+  { path: 'authors', component: AuthorList },
+  { path: 'add-book', component: BookForm }
 ];
