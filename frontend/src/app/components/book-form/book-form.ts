fb1d7e711a254056282bd7d5b50899a883cbe7cc
Matías <mcambar2610@g.educaand.es>
2026-05-20 12:28:57 +0200

    book-form

--- frontend/src/app/components/book-form/book-form.ts
+++ frontend/src/app/components/book-form/book-form.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './book-form.html',
   styleUrl: './book-form.css',
 })
 export class BookForm {
 
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    title: ['', Validators.required],
+    author: ['', Validators.required],
+  });
+
+  submitted = signal(false);
+  loading = signal(false);
+
+  submit() {
+    this.submitted.set(true);
+
+    if (this.form.invalid) return;
+
+    this.loading.set(true);
+
+    console.log('Libro:', this.form.value);
+
+    setTimeout(() => {
+      this.loading.set(false);
+      this.form.reset();
+      this.submitted.set(false);
+    }, 800);
+  }
 }
